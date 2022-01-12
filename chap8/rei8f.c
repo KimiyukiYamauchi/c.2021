b51899abe3ed6ee6b8d04bf31babb579adf1bcfc
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2022-01-12 16:28:40 +0900

    sjis->utf-8に文字コードを変換

--- chap8/rei8f.c
+++ chap8/rei8f.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
   char buf[256], *p_buf;
   int cnt;
 
-  printf("•¶š—ñH");
-  // fgets(buf, 256, stdin);
-  gets(buf);
+  printf("æ–‡å­—åˆ—ï¼Ÿ");
+  fgets(buf, 256, stdin);
+  // gets(buf);
 
   for (cnt = 0, p_buf = buf; *p_buf != '\0'; cnt++, p_buf++)
     ;
-  printf("“ü—Í•¶š” = %d\n", cnt);  
+  printf("å…¥åŠ›æ–‡å­—æ•° = %d\n", cnt);
 }
